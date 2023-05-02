72c4c7b0005fac0f77d4f7f6f9a30df316009aff
JohnathanCh <johnchriest@gmail.com>
2023-05-02 15:38:28 -0700

    done

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,33 @@
-class MySet:
+import pdb
 
-    pass
+class MySet:
+    def __init__(self, list =[]):
+        self.dictionary = {}
+        for i in list:
+            self.dictionary[i] = True
+            
+    def has(self, value):
+        return value in self.dictionary
+            
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
+    
+    def clear(self):
+        self.dictionary.clear()
+        return self
+    
+    def __str__(self):
+        # pdb.set_trace()
+        # list = []
+        # for num in set(self.dictionary.keys()):
+        #     list.append(num)
+        allKeys = [str(num) for num in self.dictionary.keys()]
+        return f'MySet: {{{",".join(allKeys)}}}'
